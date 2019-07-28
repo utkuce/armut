e42e521c01c29a1c2e4f2d4521388789d7137fdd
utku <utkuce@gmail.com>
2019-07-27 22:16:47 -0500

    Empty mod

---
+++ src/main/java/utku/armutmod/ArmutMod.java
@@ -0,0 +1,31 @@
+package utku.armutmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ArmutMod.MODID, name = ArmutMod.NAME, version = ArmutMod.VERSION)
+public class ArmutMod
+{
+    public static final String MODID = "armutmod";
+    public static final String NAME = "Armut Mod";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        //logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("Armut pis agzima dus");
+    }
+}
