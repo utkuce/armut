74d739c169e3232adb2cea2411a4b1b160e7704d
utku <utkuce@gmail.com>
2019-07-28 23:53:21 -0500

    Refactor

--- src/main/java/utku/armutmod/ArmutMod.java
+++ src/main/java/utku/armutmod/ArmutMod.java
@@ -7,18 +7,18 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ArmutMod.MODID, name = ArmutMod.NAME, version = ArmutMod.VERSION)
+@Mod(modid = ArmutMod.MOD_ID, name = ArmutMod.NAME, version = ArmutMod.VERSION)
 public class ArmutMod implements IProxy {
 
     @SidedProxy(clientSide = "utku.armutmod.ClientCode", serverSide = "utku.armutmod.ServerCode")
     private static ArmutMod proxy;
 
-    public static final String MODID = "armutmod";
-    public static final String NAME = "Armut Mod";
-    public static final String VERSION = "0.1.0";
+    static final String MOD_ID = "armutmod";
+    static final String NAME = "Armut Mod";
+    static final String VERSION = "0.1.0";
 
-    protected static Logger logger;
-    protected static MyLogger mylogger;
+    static Logger logger;
+    static MyLogger mylogger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
