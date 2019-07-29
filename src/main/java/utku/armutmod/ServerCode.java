79e38c89182fca250019728d5b4c4b006107c91e
utku <utkuce@gmail.com>
2019-07-29 00:13:48 -0500

    Add mods_list.txt generation on server

--- src/main/java/utku/armutmod/ServerCode.java
+++ src/main/java/utku/armutmod/ServerCode.java
@@ -1,10 +1,19 @@
 package utku.armutmod;
 
+import fi.iki.elonen.SimpleWebServer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class ServerCode extends ArmutMod implements IProxy{
 
@@ -25,5 +34,31 @@ public class ServerCode extends ArmutMod implements IProxy{
     @SideOnly(Side.SERVER)
     private void serverCode(){
         logger.info("Running in server mode");
+        logger.info("Armut pis agzima dus");
+
+        //SimpleWebServer.main(null);
+
+        try {
+            listDirectoryToFile("mods", "mods_list.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listDirectoryToFile(String folderName, String outputFile) throws IOException {
+
+        File folder = new File(folderName);
+        File[] listOfFiles = folder.listFiles();
+
+        FileWriter fw = new FileWriter(outputFile);
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                fw.write(file.getName() + System.lineSeparator());
+                logger.info("Adding " + file.getName() + " to " + outputFile);
+            }
+        }
+
+        fw.close();
     }
 }
