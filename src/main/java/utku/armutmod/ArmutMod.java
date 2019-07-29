fa779719ab5fe166661a75af6f157a636b16ea2b
utku <utkuce@gmail.com>
2019-07-28 20:33:05 -0500

    Add downloading mods from server

--- src/main/java/utku/armutmod/ArmutMod.java
+++ src/main/java/utku/armutmod/ArmutMod.java
@@ -4,7 +4,15 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 @Mod(modid = ArmutMod.MODID, name = ArmutMod.NAME, version = ArmutMod.VERSION)
 public class ArmutMod
@@ -14,6 +22,8 @@ public class ArmutMod
     public static final String VERSION = "0.1";
 
     private static Logger logger;
+
+    String serverAddress = "localhost:8080";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -26,6 +36,46 @@ public class ArmutMod
     {
         // some example code
         //logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
         logger.info("Armut pis agzima dus");
+        logger.info("Getting list of mods");
+
+        try {
+            URL url = new URL("http://" + serverAddress + "/mods_list.txt");
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()) {
+                String modName = scanner.next();
+
+                logger.info("Checking mod: " + modName);
+                if (Files.exists(Paths.get("mods/" + modName))) {
+                    logger.info("Mod exists, skipping download");
+                }
+                else {
+                    downloadMod(modName);
+                }
+
+            }
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void downloadMod(String modName) {
+
+        logger.info("Downloading mod: " + modName);
+        try {
+
+            FileUtils.copyURLToFile(
+
+                    new URL("http://" + serverAddress + "/mods/" + modName),
+                    new File("mods/" + modName)
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
